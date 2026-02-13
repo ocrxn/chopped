6d47750bfff1566653b92c51af4d6d7b6bb9fd40
Matthew Flanagan <matthew.flanagan>
2026-02-12 22:13:00 -0500

    updated

--- test_clipper.py
+++ test_clipper.py
@@ -1,6 +1,6 @@
 from clipper import cut_clip
 
-video_path = "uploads/game_video.mp4"
+video_path = "uploads/video.mp4"
 output_path = "clips/test_clip.mp4"
 
 cut_clip(video_path=video_path, start_time=4, duration=8, output_path=output_path)
