b3ba9488b73d2a63f8fdf95129dc87324dc22d02
Aidan-Grady <apg1063@usnh.edu>
2026-04-17 13:35:03 -0400

    Create clipper.py

---
+++ clipper.py
@@ -0,0 +1,46 @@
+#cuts a highlight clip from a video
+#uses FFmpeg when given a video path and timestamps
+
+from __future__ import annotations
+
+import subprocess #lets Python run other programs on computer (FFmpeg)
+from pathlib import Path
+import json
+import os
+
+
+#takes timestamps + labels and decides where clips start and end
+
+#gets absolute path of current Python file and returns directory it’s located in; makes BASE_DIR a path
+#__file__ is the path to the current Python file
+#.resolve turns it into an absolute path
+#.parent gets the folder containing the file
+# BASE_DIR = Path(__file__).resolve().parent
+
+#takes video + timestamps and cuts a clip
+#uses FFmpeg
+#cuts a highlight clip from a video
+#uses FFmpeg when given a video path and timestamps
+#takes in video file path
+#takes in data from JSON file from processor.py
+#uses JSON parameters
+#uses ffmpeg to cut clips and put into folders
+
+def clip_video(video_path: str, output_path: str, start_time: int, duration: int = 35):
+    video_path = os.path.abspath(video_path)
+    output_path = os.path.abspath(output_path)
+
+    if not os.path.exists(video_path):
+        raise FileNotFoundError("input video not found: {video_path}")
+    command = [
+        "ffmpeg",
+        "-ss", str(start_time), #start cutting at start time
+        "-i", video_path, #input video file
+        "-t", str(duration), #duration of the cut
+        "-c", "copy", #copies audio and video streams without re-encoding
+                      #makes process faster
+        "-y", #so ffmpeg won't stop if file already exists
+        output_path #output path for cut video file
+    ]
+
+    subprocess.run(command, check=True)
